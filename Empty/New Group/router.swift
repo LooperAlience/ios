1eacc2c85e785c141c67dfd53519ec593dbc52df
bsidesoft <bsidesoft@bsidesoftui-mac-mini.local>
2018-03-11 04:12:22 +0900

    a

---
+++ Empty/New Group/router.swift
@@ -0,0 +1,59 @@
+//
+//  router.swift
+//  Empty
+//
+//  Created by bsidesoft on 2018. 3. 8..
+//  Copyright © 2018년 com.bsidesoft.ios. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Router{
+	typealias ctrl = ()->UIViewController
+	public enum type{case add, replace, cover}
+	class Routed{
+		let router:Router, base:UIViewController, type:Router.type
+		init(_ r:Router, _ b:UIViewController, _ ty:Router.type){
+			router = r
+			base = b
+			type = ty
+		}
+		func with(_ k:String){
+			if let f = router[k] {
+				run(f())
+			}
+		}
+		func with(_ c:UIViewController){
+			run(c)
+		}
+		func with(_ f:ctrl){
+			run(f())
+		}
+		private func run(_ c:UIViewController){
+			if let base = base as? UINavigationController{
+			}else if let base = base as? UITabBarController{
+			}else{
+				base.pushRouter(c, type)
+			}
+		}
+	}
+	
+	static private var router:[String:Router] = [:]
+	
+	private let table = NSMutableDictionary()//:[String:ctrl] = [:]
+	
+	static func get(_ key:String)->Router?{
+		return Router.router[key]
+	}
+	init(_ key:String){
+		Router.router[key] = self
+	}
+	subscript(key:String)->ctrl?{
+		get{return table[key] as? ctrl}
+		set{table[key] = newValue}
+	}	
+	func route(_ c:UIViewController, _ t:Router.type)->Routed{
+		return Routed(self, c, t)
+	}
+}
